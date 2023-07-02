1cabc48d8ae2cf344062257ee1f9cbd13c1af436
ApexdaGamer <63085708+ApexdaGamer@users.noreply.github.com>
2023-07-01 23:55:18 -0700

    alpha 0.1 - done
    oh yeah yeah

--- bdf.lua
+++ bdf.lua
@@ -52,7 +52,7 @@ Frame1.BorderColor = BrickColor.new("Really black")
 Frame1.BorderColor3 = Color3.new(0, 0, 0)
 Frame1.BorderSizePixel = 0
 Frame1.ZIndex = 0
-local Gradient1 = Instance.new("Gradient1")
+local Gradient1 = Instance.new("UIGradient")
 Gradient1.Name = "Gradient1"
 Gradient1.Color = ColorSequence.new({
 	ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 0, 0)),
